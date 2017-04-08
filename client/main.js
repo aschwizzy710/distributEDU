72ff498e46da54cc58ca4c10cb615bc099990cb2
Aaron Schwartz <aschwartz628@gmail.com>
2017-04-08 18:19:11 -0400

    tried to implement one page iron router

--- client/main.js
+++ client/main.js
@@ -1,12 +1,12 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import landingPage from '../imports/components/landingPage/landingPage';
-// import intro from '../imports/components/intro/intro';
 
 angular.module('learn-bitcoin', [
   angularMeteor,
   todosList.name
 ]);
+
 
 Router.route('/', function () {
   this.render('landingPage');
--- imports/components/intro/intro.js
+++ imports/components/intro/intro.js
@@ -1,24 +1,24 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import template from './intro.html';
-
-class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
-  }
-}
-
-
-export default angular.module('learn-bitcoin', [
-  angularMeteor
-])
-  .component('landingPage', {
-    templateUrl: 'imports/components/intro/intro.html',
-    controller: TodosListCtrl
-  });
+// import angular from 'angular';
+// import angularMeteor from 'angular-meteor';
+// import template from './intro.html';
+//
+// class TodosListCtrl {
+//   constructor() {
+//     this.tasks = [{
+//       text: 'This is task 1'
+//     }, {
+//       text: 'This is task 2'
+//     }, {
+//       text: 'This is task 3'
+//     }];
+//   }
+// }
+//
+//
+// export default angular.module('learn-bitcoin', [
+//   angularMeteor
+// ])
+//   .component('landingPage', {
+//     templateUrl: 'imports/components/intro/intro.html',
+//     controller: TodosListCtrl
+//   });
